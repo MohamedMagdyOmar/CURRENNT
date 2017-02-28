d3c4fbf9f1ab69d166d9a28d5d7700066583b771
mmagdy88 <mohamedmagdyomar@gmail.com>
2017-02-28 21:39:04 +0200

    First Commit

---
+++ build/CMakeFiles/currennt.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mohamed/Desktop/NewLibrary/currennt/CMakeFiles/3.5.1/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt/CMakeFiles/3.5.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt/src/main.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt/src/main.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt_lib/src/Configuration.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt_lib/src/Configuration.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt_lib/src/NeuralNetwork.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt_lib/src/NeuralNetwork.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt_lib/src/data_sets/DataSet.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt_lib/src/data_sets/DataSet.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt_lib/src/data_sets/DataSetFraction.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt_lib/src/data_sets/DataSetFraction.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt_lib/src/helpers/JsonClasses.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt_lib/src/helpers/JsonClasses.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt_lib/src/layers/InputLayer.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt_lib/src/layers/InputLayer.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt_lib/src/layers/Layer.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt_lib/src/layers/Layer.cpp.o"
+  "/home/mohamed/Desktop/NewLibrary/currennt_lib/src/layers/PostOutputLayer.cpp" "/home/mohamed/Desktop/NewLibrary/build/CMakeFiles/currennt.dir/currennt_lib/src/layers/PostOutputLayer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/currennt.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/currennt.dir/currennt_lib/src/optimizers/currennt_generated_Optimizer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/optimizers/currennt_generated_SteepestDescentOptimizer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_SseMaskPostOutputLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_WeightedSsePostOutputLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_CePostOutputLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_MulticlassClassificationLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_SsePostOutputLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_LstmLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_RmsePostOutputLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_FeedForwardLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_SoftmaxLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_BinaryClassificationLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/currennt_generated_TrainableLayer.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/currennt_generated_LayerFactory.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/helpers/currennt_generated_Matrix.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/helpers/currennt_generated_cublas.cu.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/Configuration.cpp.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/PostOutputLayer.cpp.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/Layer.cpp.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/layers/InputLayer.cpp.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/data_sets/DataSetFraction.cpp.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/data_sets/DataSet.cpp.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/helpers/JsonClasses.cpp.o"
+  "CMakeFiles/currennt.dir/currennt_lib/src/NeuralNetwork.cpp.o"
+  "CMakeFiles/currennt.dir/currennt/src/main.cpp.o"
+  "CMakeFiles/currennt.dir/currennt/CMakeFiles/3.5.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "currennt.pdb"
+  "currennt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/currennt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
